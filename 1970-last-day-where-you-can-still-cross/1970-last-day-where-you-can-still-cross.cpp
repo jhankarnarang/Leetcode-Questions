c8774b8b14e69456bf80f6404e6c305c5b59f283
Jhankar Narang <85575169+jhankarnarang@users.noreply.github.com>
2023-06-30 18:44:05 +0530

    Time: 666 ms (51.80%), Space: 176.5 MB (50.00%) - LeetHub

---
+++ 1970-last-day-where-you-can-still-cross/1970-last-day-where-you-can-still-cross.cpp
@@ -0,0 +1,55 @@
+#include <vector>
+
+class Solution {
+private:
+    std::vector<std::vector<int>> directions = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+public:
+    bool canCross(int row, int col, std::vector<std::vector<int>>& cells, int day) {
+        std::vector<std::vector<int>> grid(row, std::vector<int>(col, 0));
+        for (int i = 0; i < day; ++i) {
+            int r = cells[i][0] - 1, c = cells[i][1] - 1;
+            grid[r][c] = 1;
+        }
+        
+        for (int i = 0; i < day; ++i) {
+            grid[cells[i][0] - 1][cells[i][1] - 1] = 1;
+        }
+        
+        for (int i = 0; i < col; ++i) {
+            if (grid[0][i] == 0 && dfs(grid, 0, i, row, col)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool dfs(std::vector<std::vector<int>>& grid, int r, int c, int row, int col) {
+        if (r < 0 || r >= row || c < 0 || c >= col || grid[r][c] != 0) {
+            return false;
+        }
+        if (r == row - 1) {
+            return true;
+        }
+        grid[r][c] = -1;
+        for (auto& dir : directions) {
+            int newR = r + dir[0], newC = c + dir[1];
+            if (dfs(grid, newR, newC, row, col)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    int latestDayToCross(int row, int col, std::vector<std::vector<int>>& cells) {
+        int left = 1, right = row * col;
+        while (left < right) {
+            int mid = right - (right - left) / 2;
+            if (canCross(row, col, cells, mid)) {
+                left = mid;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+};
